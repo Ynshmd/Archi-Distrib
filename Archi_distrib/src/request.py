03c5f3a507172b6ba7a76f4d9cf05eb171071b87
Ynshmd <118398845+Ynshmd@users.noreply.github.com>
2023-04-06 03:25:08 +0200

    Archi_distrib

---
+++ Archi_distrib/src/request.py
@@ -0,0 +1,53 @@
+import requests
+from kafka import KafkaProducer
+import time
+import json
+
+
+
+# Velib API endpoint URL
+API_URL = "https://opendata.paris.fr/api/records/1.0/search/?dataset=velib-disponibilite-en-temps-reel&q=&rows=500&sort=duedate&facet=name&facet=is_installed&facet=is_renting&facet=is_returning&facet=nom_arrondissement_communes&facet=duedate&refine.nom_arrondissement_communes=Paris&refine.is_installed=OUI&timezone=Europe%2FParis"
+
+
+# Kafka topic to send data to
+TOPIC_NAME = "topic_test"
+
+
+
+
+# Spark master URL and port
+SPARK_MASTER_URL = "spark://d8dabd93e04c:7077"
+
+# Create a Kafka producer instance
+producer = KafkaProducer(bootstrap_servers='localhost:9092')
+
+# Function to fetch Velib data from the API
+def get_velib_data():
+    response = requests.get(API_URL)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
+
+# Function to send Velib data to Kafka
+# def send_velib_data_to_kafka(velib_data):
+#     producer.send(TOPIC_NAME, velib_data.encode('utf-8'))
+
+
+def send_velib_data_to_kafka(velib_data):
+    producer = KafkaProducer(bootstrap_servers=['localhost:9092'])
+    producer.send(TOPIC_NAME, json.dumps(velib_data).encode('utf-8'))
+
+
+
+# Loop to continuously fetch and send Velib data to Kafka
+while True:
+   velib_data = get_velib_data()
+   if velib_data:
+       send_velib_data_to_kafka(velib_data)
+       time.sleep(2) # wait for 2 second before sending next message
+       print(velib_data) 
+  
+
+        
+    
---
+++ Archi_distrib/src/requests2.py
@@ -0,0 +1,47 @@
+import requests
+from kafka import KafkaProducer
+import time
+import json
+
+
+
+
+API_URL = "https://public.opendatasoft.com/api/records/1.0/search/?dataset=donnees-synop-essentielles-omm&q=&sort=date&facet=date&facet=nom&facet=temps_present&facet=libgeo&facet=nom_epci&facet=nom_dept&facet=nom_reg&refine.date=2023"
+
+
+
+TOPIC_NAME = "topic_test2"
+
+
+
+
+
+SPARK_MASTER_URL = "spark://d8dabd93e04c:7077"
+
+
+producer = KafkaProducer(bootstrap_servers='localhost:9092')
+
+
+def get_temps():
+    response = requests.get(API_URL)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
+
+
+
+
+def send_temps_kafka(temps_data):
+    producer = KafkaProducer(bootstrap_servers=['localhost:9092'])
+    producer.send(TOPIC_NAME, json.dumps(temps_data).encode('utf-8'))
+
+
+
+
+while True:
+   temps_data = get_temps()
+   if temps_data:
+       send_temps_kafka(temps_data)
+       time.sleep(2) 
+       print(temps_data) 
